2c7911ad8c50e0b437e6aaaadc3b1503b5e1c044
(init.dp.ua) Кулинич Вячеслав <kulynych.v.a@init.dp.ua>
2017-09-14 10:35:50 +0300

    new tabbed adapter

---
+++ app/src/main/java/com/example/slava/bmigraph/adapters/TabbedFragmentAdapter.java
@@ -0,0 +1,46 @@
+package com.example.slava.bmigraph.adapters;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.example.slava.bmigraph.MainActivity;
+import com.example.slava.bmigraph.fragments.BMIFragment;
+import com.example.slava.bmigraph.fragments.HeightGraphFragment;
+import com.example.slava.bmigraph.fragments.WeightGraphFragment;
+
+import java.util.List;
+
+public class TabbedFragmentAdapter extends FragmentPagerAdapter {
+    private List<String> mList;
+
+    public TabbedFragmentAdapter(FragmentManager fragmentManager, List<String> mList) {
+        super(fragmentManager);
+        this.mList = mList;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mList.get(position);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return BMIFragment.getInstance();
+            case 1:
+                return WeightGraphFragment.getInstance();
+            case 2:
+                return HeightGraphFragment.getInstance();
+        }
+
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return mList.size();
+    }
+}
