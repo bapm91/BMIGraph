9261c8640db245acd3b3f8b6b6d6c8a3f8d00844
(init.dp.ua) Кулинич Вячеслав <kulynych.v.a@init.dp.ua>
2017-09-14 10:34:28 +0300

    new tabbed adapter

--- app/src/main/java/com/example/slava/bmigraph/MainActivity.java
+++ app/src/main/java/com/example/slava/bmigraph/MainActivity.java
@@ -1,66 +1,48 @@
 package com.example.slava.bmigraph;
 
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 
-import android.widget.TextView;
+import com.example.slava.bmigraph.adapters.TabbedFragmentAdapter;
 
-import com.example.slava.bmigraph.fragments.BMIFragment;
-import com.example.slava.bmigraph.fragments.HeightGraphFragment;
-import com.example.slava.bmigraph.fragments.WeightGraphFragment;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
-    private SectionsPagerAdapter mSectionsPagerAdapter;
-
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
-    private ViewPager mViewPager;
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
 
-        initViewPager();
-
-
-
+        initTabbedActivity(getSupportActionBar());
     }
 
-    private void initViewPager() {
+    private void initTabbedActivity(ActionBar actionBar) {
+        List<String> list = new ArrayList<>();
+        list.add(getString(R.string.title_bmi_fragment));
+        list.add(getString(R.string.title_weight_fragment));
+        list.add(getString(R.string.title_height_fragment));
+
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        TabbedFragmentAdapter tabbed =
+                new TabbedFragmentAdapter(getSupportFragmentManager(), list, this);
 
         // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.container);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
+        ViewPager viewPager = (ViewPager) findViewById(R.id.container);
+        viewPager.setAdapter(tabbed);
     }
 
     @Override
@@ -83,49 +65,5 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            switch (position){
-                case 0:
-                    return BMIFragment.getInstance();
-                case 1:
-                    return WeightGraphFragment.getInstance();
-                case 2:
-                    return HeightGraphFragment.getInstance();
-            }
-
-            return null;
-        }
-
-        @Override
-        public int getCount() {
-            // Show 3 total pages.
-            return 3;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            switch (position) {
-                case 0:
-                    return "SECTION 1";
-                case 1:
-                    return "SECTION 2";
-                case 2:
-                    return "SECTION 3";
-            }
-            return null;
-        }
     }
 }
